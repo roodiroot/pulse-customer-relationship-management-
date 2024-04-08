294bba0830392108720a6b9d1964888a1f8c93b4
RoodiRoot <borisov130490@gmail.com>
2024-04-08 19:09:45 +0300

    fix:6

--- actions/contact/update-completed-contact.ts
+++ actions/contact/update-completed-contact.ts
@@ -11,14 +11,14 @@ export const updateComletedContact = async (
   if (!confirmed) {
     return { error: "Некорректные данные" };
   }
-  try {
-    await db.contact.update({
-      where: { id: contactId },
-      data: { confirmed: false },
-    });
-    revalidatePath("/companies/[id]/contacts/[contactId]", "page");
-    revalidatePath("/companies/[id]", "page");
-  } catch {
-    return null;
-  }
+  // try {
+  //   await db.contact.update({
+  //     where: { id: contactId },
+  //     data: { confirmed: false },
+  //   });
+  //   revalidatePath("/companies/[id]/contacts/[contactId]", "page");
+  //   revalidatePath("/companies/[id]", "page");
+  // } catch {
+  //   return null;
+  // }
 };
