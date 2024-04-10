2433536c3f685cdc86146375a3685653134ce7ae
RoodiRoot <borisov130490@gmail.com>
2024-04-10 09:48:41 +0300

    fix:2

--- components/page/company-page/contact-item-info.tsx
+++ components/page/company-page/contact-item-info.tsx
@@ -1,5 +1,7 @@
 import InputPhoneMask from "@/components/mask/input-phone-mask";
 import { Input } from "@/components/ui/input";
+import { CheckCheck, Copy, CopyCheck, CopyCheckIcon } from "lucide-react";
+import { useState } from "react";
 
 interface ContactItemInfoProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
@@ -15,18 +17,40 @@ const ContactItemInfo: React.FC<ContactItemInfoProps> = ({
   comment,
   ...props
 }) => {
+  const [copy, setCopy] = useState(false);
+  const copyTextToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopy(true);
+    } catch (err) {
+      console.error("Ошибка:", err);
+    }
+  };
   return (
-    <div className="space-y-3">
+    <div {...props} className="space-y-3">
       <input
         defaultValue={name}
         disabled
         className="w-full  px-4 py-2 focus-visible:outline-none rounded-md disabled:bg-inherit  h-10"
       />
-      <input
-        defaultValue={phone}
-        disabled
-        className="w-full  px-4 py-2 focus-visible:outline-none rounded-md disabled:bg-inherit  h-10"
-      />
+      <div className="w-full h-10 flex gap-4 items-center">
+        <input
+          defaultValue={phone}
+          disabled
+          className="max-w-[150px] h-full bg-inherit  px-4 py-2 focus-visible:outline-none rounded-md disabled:bg-inherit  "
+        />
+        {copy ? (
+          <CheckCheck
+            className="w-4 h-4 text-green-400 cursor-pointer"
+            onClick={() => copyTextToClipboard(phone)}
+          />
+        ) : (
+          <Copy
+            className="w-4 h-4 text-muted-foreground cursor-pointer"
+            onClick={() => copyTextToClipboard(phone)}
+          />
+        )}
+      </div>
       <input
         defaultValue={mail}
         disabled
