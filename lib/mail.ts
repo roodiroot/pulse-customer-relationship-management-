7235c1a9b4a1eaeffadde57ed8a5803b4ca4cf8c
maxim <borisov130490@gmail.com>
2024-10-13 18:49:02 +0300

    change  type for transporter

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,12 +3,12 @@ import { transporter } from "@/lib/sender-transporter";
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.PUBLIC_URL}/auth/new-verification?token=${token}`;
   // send mail with defined transport object
-  const info = await transporter.sendMail({
+  const info: { messageId?: string } = (await transporter.sendMail({
     from: process.env.MAIL_USER, // sender address
     to: email, // list of receivers
     subject: "Подтверждение email ✔", // Subject line
     html: `<p>Для подтверждения нажми <a href="${confirmLink}">тут</a></p>`, // html body
-  });
+  })) as { messageId?: string };
   if (info?.messageId) return { success: "Сообщение отправлено", status: 200 };
 
   return { error: "Ошибка отправки" };
@@ -17,12 +17,12 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${process.env.PUBLIC_URL}/auth/new-password?token=${token}`;
   // send mail with defined transport object
-  const info = await transporter.sendMail({
+  const info = (await transporter.sendMail({
     from: process.env.MAIL_USER, // sender address
     to: email, // list of receivers
     subject: "Сброс пароля ✔", // Subject line
     html: `<p>Для сброса пароля нажми <a href="${resetLink}">тут</a></p>`, // html body
-  });
+  })) as { messageId?: string };
   if (info?.messageId) return { success: "Сообщение отправлено", status: 200 };
 
   return { error: "Ошибка отправки" };
